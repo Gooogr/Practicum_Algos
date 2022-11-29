c82a3a76d18a954c861dff1a3856b83799779451
Gooogr <gusarovga@ya.ru>
2022-11-29 22:58:39 +0300

    feat: add tasks 1_A, 1_B

---
+++ Sprint_1/B.py
@@ -0,0 +1,11 @@
+def check_parity(a: int, b: int, c: int) -> bool:
+	return (a%2)==(b%2)==(c%2)
+
+def print_result(result: bool) -> None:
+    if result:
+        print("WIN")
+    else:
+        print("FAIL")
+
+a, b, c = map(int, input().strip().split())
+print_result(check_parity(a, b, c))
