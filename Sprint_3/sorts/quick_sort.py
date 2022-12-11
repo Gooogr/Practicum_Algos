c9d79f6b2e026d2738fab6303b15ec533fb16f2a
Gooogr <gusarovga@ya.ru>
2022-12-11 19:39:51 +0300

    feat: add quck sort example

---
+++ Sprint_3/sorts/quick_sort.py
@@ -0,0 +1,25 @@
+# O(n*log(n))
+
+from typing import List, Tuple
+
+def partition(
+    array: List[int], 
+    pivot:int
+) -> Tuple[List[int], List[int], List[int]]:
+    left = [x for x in array if x < pivot]
+    center = [x for x in array if x == pivot]
+    right = [x for x in array if x > pivot]
+    return left, center, right
+
+def quicksort(array: List[int]) -> List[int]:
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        left, center, right = partition(array, pivot)
+        return quicksort(left) + center + quicksort(right)
+
+arr = list(map(int, input().strip().split()))
+result = quicksort(arr)
+print('Input:', arr)
+print('Result:', result)
