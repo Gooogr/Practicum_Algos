d6ce87afd1c3944e73bbdf3c0c1e53a82085f6ec
Gooogr <gusarovga@ya.ru>
2022-12-22 18:41:50 +0300

    feat: add task 4_F

---
+++ Sprint_4/train/F.py
@@ -0,0 +1,31 @@
+# https://contest.yandex.ru/contest/23991/problems/F/
+
+from typing import List
+from collections import defaultdict
+
+def get_string_code(s: str):
+    code_list = [0] * 26 
+    for letter in s:
+        ord_idx = ord(letter) - ord('a')
+        code_list[ord_idx] += 1
+    return str(code_list)
+
+
+def group_anagrams(words: List[str]):
+    anagram_dict = defaultdict(list)
+    for idx in range(len(words)):
+        word_code = get_string_code(words[idx])
+        anagram_dict[word_code].append(idx)
+    return sorted(anagram_dict.values())
+
+    
+
+
+    
+
+def read_input():
+    _ = input()
+    words = input().split()
+    return words
+
+print(group_anagrams(read_input()))
