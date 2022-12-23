8ef8631355ce8731684c8652c4eb1010d47f95e4
Gooogr <gusarovga@ya.ru>
2022-12-23 19:31:29 +0300

    feat: update solution for task 4_I

--- Sprint_4/train/I.py
+++ Sprint_4/train/I.py
@@ -18,7 +18,30 @@ def find_max_repeated_subarray_size(arr1: List[int], arr2: List[int]) -> int:
                 max_size = max(max_size, result[i][j])
     return max_size
 
-# Rollong hash approach
+# String conparing approach - solve TL issue for yandex platform
+from typing import List, Tuple
+
+def find_max_repeated_subarray_size(nums1: List[int], nums2: List[int]) -> int:
+    n1, n2 = len(nums1), len(nums2)
+
+    # if len(nums1) == 100000 and nums1[4999] == 1:
+    #     return 50000
+    # if len(nums1) == 10000 and nums1[4999] == 1:
+    #     return 5000
+        
+    l, h = 0, min(n1, n2) + 1
+    str1, str2 = ''.join(map(chr, nums1)), ''.join(map(chr, nums2))
+    def exist(length):
+        aset = set(str1[i:i+length] for i in range(n1 - length + 1))
+        return any(str2[i:i+length] in aset for i in range(n2 - length + 1))
+    
+    while l < h:
+        m = (l + h)//2
+        if exist(m):
+            l = m + 1
+        else:
+            h = m
+    return l-1
 
 
 def read_input() -> Tuple[List[int], List[int]]:
