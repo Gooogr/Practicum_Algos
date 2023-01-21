c399478a33d3195120285b6904b3eadd8383e65c
Gooogr <gusarovga@ya.ru>
2023-01-22 01:19:57 +0300

    feat: add final task 5_A

--- Sprint_5/final/A.py
+++ Sprint_5/final/A.py
@@ -1,5 +1,8 @@
 # https://contest.yandex.ru/contest/24810/problems/A/
 
+from typing import List, Tuple, Hashable
+from dataclasses import dataclass    
+
 class MaxHeap():
     def __init__(self):
         self.size = 0
@@ -18,9 +21,9 @@ class MaxHeap():
             self.heap[parent_idx], self.heap[idx] = self.heap[idx], self.heap[parent_idx]
             return self._sift_up(parent_idx)  
 
-    def _max_child(self, idx):
+    def _max_child(self, idx: int):
         '''
-        Find max child index of selected heap node.
+        Find max child index of the selected heap node.
         Return:
             If node hasn't any child - None
             If only left child - index of left child
@@ -50,11 +53,17 @@ class MaxHeap():
             idx = idx_largest
     
     def insert(self, x: int) -> None:
+        '''
+        Add new value to the heap
+        '''
         self.heap.append(x)
         self.size += 1
         self._sift_up(self.size)
 
-    def pop(self) -> int:
+    def pop(self) -> Hashable:
+        '''
+        Remove root value from the heap and update tree
+        '''
         if self.size < 1:
             raise ValueError("Can't remove element, heap is empty")
         max_value = self.heap[1]
@@ -64,16 +73,25 @@ class MaxHeap():
         self.size -= 1
         self._sift_down(1)
         return max_value
+   
+def read_input() -> List[Tuple[str, int, int]]:
+    n = int(input())
+    input_list = []
+    for _ in range(n):
+        login, number, penalty = list(input().strip().split())
+        input_list.append((-int(number), int(penalty), login))
+    return input_list
 
+def heap_sort(x: List) -> List:
+    heap = MaxHeap()
+    sorted_result = []
+    for item in x:
+        heap.insert(item)
+    while heap.size > 0:
+        sorted_result.append(heap.pop())
+    return sorted_result
 
-min_heap = MaxHeap()
-min_heap.insert(10)
-min_heap.insert(20)
-min_heap.insert(30)
-min_heap.insert(-100)
-min_heap.pop()
-min_heap.pop()
-min_heap.pop()
-min_heap.pop()
+result = heap_sort(read_input())
+for competitor in result[::-1]:
+    print(competitor[2])
 
-print(min_heap.heap, min_heap.size)
