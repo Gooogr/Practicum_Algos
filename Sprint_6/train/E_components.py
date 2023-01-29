671d95a4cfa495c9ee2ec1130d384b4480498bc9
Gooogr <gusarovga@ya.ru>
2023-01-30 01:59:41 +0300

    feat: add task 6_E with recursive solution

---
+++ Sprint_6/train/E_components.py
@@ -0,0 +1,58 @@
+# https://contest.yandex.ru/contest/25069/problems/E/
+
+from typing import List, Dict, Tuple
+from itertools import groupby
+from collections import defaultdict
+
+
+def read_input() -> Tuple[int, List[Tuple[int, int]]]:
+    num_v, num_e = list(map(int, input().split()))
+    edges = []
+    for _ in range(num_e):
+        u, v = list(map(int, input().split()))
+        edges.append((u, v))
+        edges.append((v, u))
+    return edges, num_v
+
+def create_adj_list(num_v: int, edges: List[Tuple[int, int]]) -> Dict[int,list]:
+    adj_list = {}
+    # {v1: [u1, u2, ...], ...}
+    for vertex, edge_group in groupby(sorted(edges), lambda x: x[0]):
+        adj_list[vertex] = [edge[1] for edge in edge_group]
+    # add vertexes without edges
+    for vertex in range(1, num_v + 1):
+        adj_list[vertex] = adj_list.get(vertex, [])
+    return adj_list
+
+
+edges, num_v = read_input()
+adj_list = create_adj_list(num_v, edges)
+
+# Components initialization
+components = [-1] * (len(adj_list) + 1)
+components[0] = None
+component_count = 1
+
+def dfs_traversal(v, adj_list):
+    components[v] = component_count
+    for w in adj_list[v]:
+        if components[w] == -1:
+            dfs_traversal(w, adj_list)
+
+def main_components(adj_list):
+    global component_count
+    for v in adj_list.keys():
+        if components[v] == -1:
+            dfs_traversal(v, adj_list)
+        component_count += 1
+
+main_components(adj_list)
+result = defaultdict(list)
+for idx, value in enumerate(components[1:]):
+    result[value].append(idx + 1)
+
+sorted_nodes = sorted(result.values())
+
+print(len(sorted_nodes))
+for item in sorted_nodes:
+    print(*item)
