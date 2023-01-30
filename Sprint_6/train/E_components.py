399b082d19c3605825cbdbdbafd527adf37b4bcd
Gooogr <gusarovga@ya.ru>
2023-01-30 15:40:38 +0300

    feat: add iterative dfs approach. Still TL errors

--- Sprint_6/train/E_components.py
+++ Sprint_6/train/E_components.py
@@ -28,30 +28,64 @@ def create_adj_list(num_v: int, edges: List[Tuple[int, int]]) -> Dict[int,list]:
 edges, num_v = read_input()
 adj_list = create_adj_list(num_v, edges)
 
-# Components initialization
-components = [-1] * (len(adj_list) + 1)
-components[0] = None
-component_count = 1
 
-def dfs_traversal(v, adj_list):
-    components[v] = component_count
-    for w in adj_list[v]:
-        if components[w] == -1:
-            dfs_traversal(w, adj_list)
+###------------------------------- Iterative DFS -------------------------------###
 
-def main_components(adj_list):
-    global component_count
-    for v in adj_list.keys():
-        if components[v] == -1:
-            dfs_traversal(v, adj_list)
+def dfs_iterative(adj_list):
+    components = [-1] * (len(adj_list) + 1)
+    components[0] = None
+    component_count = 1
+
+    while -1 in set(components):
+        start_vertex = components.index(-1)
+        stack = [start_vertex]
+        while stack:
+            v = stack.pop() 
+            if components[v] == -1:
+                components[v] = component_count
+                stack.append(v) # to backtrack it later
+                for w in adj_list[v]:
+                    if components[w] == -1:
+                        stack.append(w)
         component_count += 1
+        
+    return components
+components = dfs_iterative(adj_list)
 
-main_components(adj_list)
+###-----------------------------------------------------------------------------###
+
+
+###------------------------------- Recursive DFS -------------------------------###
+
+# # TL and RE for recursive approach
+# # Components initialization
+# components = [-1] * (len(adj_list) + 1)
+# components[0] = None
+# component_count = 1
+
+# def dfs_traversal(v, adj_list):
+#     components[v] = component_count
+#     for w in adj_list[v]:
+#         if components[w] == -1:
+#             dfs_traversal(w, adj_list)
+
+# def main_components(adj_list):
+#     global component_count
+#     for v in adj_list.keys():
+#         if components[v] == -1:
+#             dfs_traversal(v, adj_list)
+#         component_count += 1
+
+# main_components(adj_list)
+
+###-----------------------------------------------------------------------------###
+
 result = defaultdict(list)
 for idx, value in enumerate(components[1:]):
     result[value].append(idx + 1)
 
-sorted_nodes = sorted(result.values())
+# sorted_nodes = sorted(result.values())
+sorted_nodes = result.values()
 
 print(len(sorted_nodes))
 for item in sorted_nodes:
