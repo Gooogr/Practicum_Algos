0585d52d5a4708b40a3e20c84e4129deebf1b00a
Gooogr <gusarovga@ya.ru>
2023-02-01 22:36:07 +0300

    fix add final code version

--- Sprint_6/final/A_maximim_spanning_tree.py
+++ Sprint_6/final/A_maximim_spanning_tree.py
@@ -1,6 +1,6 @@
 # https://contest.yandex.ru/contest/25070/problems/A/
 
-from typing import List, Tuple
+from typing import List, Tuple, Set
 import heapq
 
 def read_input() -> List[List[Tuple[int, int]]]:
@@ -8,35 +8,54 @@ def read_input() -> List[List[Tuple[int, int]]]:
     adj_list = [[] for _ in range(n_vertex + 1)]
     for _ in range(n_edges):
         u, v, weight = list(map(int, input().split()))
-        adj_list[u].append((v, weight))
-        adj_list[v].append((u, weight))
+        adj_list[u].append((v, -weight)) #negative weight for result invertion
+        adj_list[v].append((u, -weight))
     return adj_list
 
-
-added = set()         
-edges = []      
-  
-
-def _add_vertex(v: int, adj_list):
+def _add_vertex(
+    v: int, 
+    adj_list: List[List[Tuple[int, int]]], 
+    added: Set[int], 
+    edges: List[Tuple[int, int]]
+) -> Tuple[Set[int], List[Tuple[int, int]]]:
+    '''
+    Helper function. Add all the edges (vertices and corresponded edges weights) that are 
+    incidental to v, but their end is not yet in the spanning tree.
+    '''
     added.add(v)
     for u, weight in adj_list[v]:
         if u not in added:
-            heapq.heappush(edges, (-weight, u))
+            heapq.heappush(edges, (weight, u))
+    return added, edges
 
 def find_MST(adj_list: List[List[Tuple[int, int]]]) -> int:
-    spaning_tree_sum = 0  
-    _add_vertex(1, adj_list) # graph can't be empty based on task description
+    '''
+    Main function. Find vertices of minimal spanning tree with
+    its total size. Based on the Prim's algorithm.
+    '''
+    added = set()          # container for all spaning tree nodes         
+    edges = []             # queue for processed vertices
+    spaning_tree_sum = 0   # total size of spaning tree 
+
+    # graph can't be empty based on task description
+    # so it's safe to inialize process it by 1 
+    added, edges = _add_vertex(1, adj_list, added, edges) 
+
+    # while we have accesible vertices to visit
     while len(added) < len(adj_list) and edges:
         min_weight, min_vertex = heapq.heappop(edges)
         if min_vertex not in added:
             spaning_tree_sum += min_weight
-            _add_vertex(min_vertex, adj_list)
-    return spaning_tree_sum
+            added, edges = _add_vertex(min_vertex, adj_list, added, edges)
+    return spaning_tree_sum, added
 
 adj_list = read_input()
-spaning_tree_sum = find_MST(adj_list)
-print(added)
-print(spaning_tree_sum)
+spaning_tree_sum, spaning_tree_edges = find_MST(adj_list)
+
+if len(spaning_tree_edges) == len(adj_list) - 1:
+    print(abs(spaning_tree_sum))
+else:
+    print('Oops! I did it again')
 
 
 
