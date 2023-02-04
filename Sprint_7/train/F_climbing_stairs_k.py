2f47d980369a4889cb3f3dfcd3dc0b0395c6c577
Gooogr <gusarovga@ya.ru>
2023-02-04 21:29:35 +0300

    feat: update code. Add DP with recusrion approach

--- Sprint_7/train/F_climbing_stairs_k.py
+++ Sprint_7/train/F_climbing_stairs_k.py
@@ -2,22 +2,31 @@
 
 ladder_size, max_steps = map(int, input().split())
 
-
-def total_ways(ladder_size, max_steps):
+# DP with recursion
+def total_ways(ladder_size: int, max_steps: int, cache:dict={}):
     # If ladder_size - curret_step < 0
     if ladder_size < 0:
         return 0
     # Base case
     if ladder_size < 2:
         return 1
- 
+
+    # Get pre-calculated value
+    if ladder_size in cache:
+        return cache[ladder_size]
+
+    # Call recursion for calculation
+    # F[n] = F[n - 1] + ... + F[n - max_steps]
     count = 0
     for i in range(1, max_steps + 1):
-        count += total_ways(ladder_size - i, max_steps)
+        count += total_ways(ladder_size - i, max_steps, cache)
+        count %= (1e9 + 7)
+    cache[ladder_size] = int(count)
  
-    return count
+    return cache[ladder_size]
 
-print(total_ways(ladder_size - 1, max_steps)) #-1 because every start from the first point
+# -1 because every participant start from the first point
+print(total_ways(ladder_size - 1, max_steps)) 
 
 
 
