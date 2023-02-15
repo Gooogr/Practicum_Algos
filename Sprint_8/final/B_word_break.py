9bff563e07ef19cc5b6acec04b90746abdeea664
Gooogr <gusarovga@ya.ru>
2023-02-15 15:45:29 +0300

    feat: add final solution for task 8_B

--- Sprint_8/final/B_word_break.py
+++ Sprint_8/final/B_word_break.py
@@ -1,5 +1,5 @@
 # https://contest.yandex.ru/contest/26133/problems/B/ 
-# 
+# https://contest.yandex.ru/contest/26133/run-report/82354911/
 
 from typing import List, Tuple
 
@@ -8,24 +8,7 @@ class TrieNode:
         self.char = char
         self.children = dict() # keys are chars, values are TrieNodes
         self.terminal = False
-        self.size = None       # len of word in terminal node
 
-class Trie:
-    def __init__(self):
-        self.root = TrieNode("")
-
-    def insert(self, word:str):
-        node = self.root
-        for char in word:
-            # if char in children - traverse to child
-            if char in node.children:
-                node = node.children[char]
-            # else - create new child and then traverse
-            else:
-                node.children[char] = TrieNode(char)
-                node = node.children[char]
-        node.terminal = True
-        node.size = len(word)
 
 def read_input() -> Tuple[str, List[str]]:
     s = input()
@@ -34,4 +17,46 @@ def read_input() -> Tuple[str, List[str]]:
     for _ in range(n):
         words.append(input())
     return s, words
+
+
+def create_trie(words: List[str]):
+    root = TrieNode("")
+    for word in words:
+        node = root            # every new word starts from root
+        for char in word:
+            # traverse trough existed chars
+            if char in node.children:
+                node = node.children[char]
+            # add new nodes and traverse trough new path
+            else:
+                node.children[char] = TrieNode(char)
+                node = node.children[char]
+            # add terminal node info
+        node.terminal = True
+    return root
+
+
+def can_split(s: str, trie: TrieNode) -> bool:
+    dp = [False] * (len(s) + 1)
+    # Base case. Empty string
+    dp[0] = True
+    for i in range(len(s)):
+        node = trie
+        # if we could get to this substring point
+        if dp[i]:                  
+            # collect info about other point where we can get from this one
+            for j in range(i, len(s) + 1):
+                if node.terminal:
+                    dp[j] = True
+                # return to root of trie if we met end of branch
+                if j == len(s) or s[j] not in node.children:
+                    break
+                node = node.children[s[j]]
+    return dp[-1]
+
+
+s, words = read_input()
+trie = create_trie(words)
+print('YES' if can_split(s, trie) else 'NO')
+
     
